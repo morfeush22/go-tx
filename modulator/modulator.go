e144c893c46ae4e7f284d01855fa106971ddf8a8
morfeush22 <patro.kacper@gmail.com>
2018-07-02 14:53:43 +0200

    basic ELK logging

--- modulator/modulator.go
+++ modulator/modulator.go
@@ -101,6 +101,14 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
+	file, err := os.OpenFile("/logging/modulator.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err == nil {
+		log.SetOutput(file)
+		defer file.Close()
+	} else {
+		log.Warn("Failed to log to file, using default stderr")
+	}
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
